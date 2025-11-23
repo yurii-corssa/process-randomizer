c2f6230d632ef31f0f899989a080043921be6690
Corssa <yurii.corssa@gmail.com>
2025-11-23 16:06:55 +0100

    Add FormButton component

---
+++ src/components/FormButton.jsx
@@ -0,0 +1,30 @@
+import { Button } from 'react-bootstrap';
+import { LuDices, LuListRestart } from 'react-icons/lu';
+
+const FormButton = ({ isRandomizing, reset }) => {
+  return (
+    <div className="d-flex gap-3 flex-wrap">
+      <Button
+        variant="primary"
+        type="submit"
+        form="randomForm"
+        className="d-flex gap-2 flex-fill justify-content-center text-nowrap"
+        disabled={isRandomizing}
+      >
+        <LuDices size={22} />
+        Go Randomize
+      </Button>
+      <Button
+        variant="secondary"
+        className="d-flex gap-2 flex-fill justify-content-center text-nowrap"
+        disabled={isRandomizing}
+        onClick={reset}
+      >
+        <LuListRestart size={22} />
+        Reset Form
+      </Button>
+    </div>
+  );
+};
+
+export default FormButton;
