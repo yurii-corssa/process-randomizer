25a0a1aeae8bd24be053d21fadca30f62eae1d89
Corssa <yurii.corssa@gmail.com>
2025-11-24 13:15:53 +0100

    Implement ConfettiExplosion in Confetti component

--- src/components/Confetti.jsx
+++ src/components/Confetti.jsx
@@ -1,5 +1,28 @@
+import ConfettiExplosion from 'react-confetti-explosion';
+
 const Confetti = () => {
-  return <div>Confetti</div>;
+  return (
+    <>
+      <ConfettiExplosion
+        force={0.8}
+        duration={3000}
+        particleCount={150}
+        style={{ position: 'absolute', top: '0', left: '0' }}
+      />
+      <ConfettiExplosion
+        force={0.8}
+        duration={3000}
+        particleCount={150}
+        style={{ position: 'absolute', top: '0', left: '100%' }}
+      />
+      <ConfettiExplosion
+        force={0.8}
+        duration={3000}
+        particleCount={150}
+        style={{ position: 'absolute', top: '50%', left: '50%' }}
+      />
+    </>
+  );
 };
 
 export default Confetti;
